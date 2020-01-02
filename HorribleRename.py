3cc2f36361d696667dfafbecc0ee2cbcd37a2b8c
AlecJDavidson <44955224+AlecJDavidson@users.noreply.github.com>
2020-01-02 14:15:07 -0500

    Updated
    Runs faster
    Less lines of code

--- HorribleRename.py
+++ HorribleRename.py
@@ -1,61 +1,24 @@
 # Name of code: AutHorribleRename
 # Author of code: Alec J. Davidson
-# Version of the code and the date of it’s last revision: ver 2.15 11/30/19
+# Version of the code and the date of it’s last revision: ver 2.15 01/02/19
 # Summary/Goal of the code: This script will help manage your anime library
 # by renaming anime from HorribleSubs to an ideal format for Plex.
 # This version will run automagically to cleanup my plex server's anime folder and all sub directories.
 
-import os	# Imports os for use of file system
-import PTN	# Allows the parsing of the raw files
+import os
+import PTN
 
-animeDir = '/home/plex/plex-media/anime' # PATH TO ANIME DIR ON SERVER
-anime = []						# Creates a list for anime directories
-anime = os.listdir(animeDir)	# Stores anime directories in the list
-series_count = -1				# Keeps count of how many anime series are in the directory
-for series in anime:			# For loop to iterate over each series in the directory
-	series_count += 1			# Adds 1 for each series in the directory
-	print(anime[series_count])	# Prints the series name
-	series=[]					# Creates a list to store the series in.
-	series = sorted(os.listdir(animeDir+'/'+str(anime[series_count])))	# Stores each series in a list
-	season_count = -1													# Keeps count of how many seasons there are
-	for season in series:												# For loop to iterate over each season in the series directory
-		season_count += 1												# Adds 1 fore each season in the directory
-		print('  ',series[season_count])								# Prints the season
-		season_path = (animeDir+'/'+str(anime[series_count]))
-		os.chdir(season_path)
-		season = (series[season_count])
-		isFile = os.path.isfile(season)		# Checks whether the element is a file
-		isDirectory = os.path.isdir(season) # Checks wheter the element is a directory
-		if isFile == True:
-			#print('Is a file')
-			name = series[season_count]		# Episode name
-			info = PTN.parse(name) 				# Parses the name of the file
-			title = info['title'] 				# Title string from the parsed file
-			container = info['container'] 		# File extention type from the parsed file
-			title = (title+'.'+container)		# Edited title and final name for the file
-			episode_path = (animeDir+'/'+str(anime[series_count])) # Defines path to the episodes in the season directory
-			os.chdir(episode_path)				# Points to target directory so that files can be changed
-			if 'HorribleSubs' in name:			# Condition to find HorribleSubs in the file name
-				os.rename(episodes,title) 		# Renames target files using new_name.
-			else:
-				print('	','File has already been renamed')
-		
-		if isDirectory == True:
-			#print('Is a directory')
-			seasons=[]								# Creates a list for each season
-			seasons = sorted(os.listdir(animeDir+'/'+str(anime[series_count])+'/'+str(series[season_count]))) # Path to seasons
-			episode_count = -1						# Keeps count of how many episodes there are
-			for episodes in seasons:				# Fore loop to iterate over each episode in the season directory
-				print('    ',seasons[episode_count])# Prints the episode name
-				episode_count += 1					# Adds 1 for each episode in the season directory
-				name = seasons[episode_count]		# Episode name
-				info = PTN.parse(name) 				# Parses the name of the file
-				title = info['title'] 				# Title string from the parsed file
-				container = info['container'] 		# File extention type from the parsed file
-				title = (title+'.'+container)		# Edited title and final name for the file
-				episode_path = (animeDir+'/'+str(anime[series_count])+'/'+str(series[season_count])) # Defines path to the episodes in the season directory
-				os.chdir(episode_path)				# Points to target directory so that files can be changed
-				if 'HorribleSubs' in name:			# Condition to find HorribleSubs in the file name
-					os.rename(episodes,title) 		# Renames target files using new_name.
-				else:
-					print('	','File has already been renamed')
+
+def rename():
+	info = PTN.parse(fName)
+	newTitle = info['title']+'.'+info['container']
+	os.rename(fName, newTitle)
+
+
+for dirName, subdirList, fileList in os.walk('/home/plex'):
+	for fName in fileList:
+		os.chdir(dirName)
+		isFile = os.path.isfile(fName)
+		if isFile == True and \
+				'[HorribleSubs]' in fName:
+			rename()
